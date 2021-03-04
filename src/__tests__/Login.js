cda220c163010ed93ec411e78bac1aa673ee4f78
Andreas Desktop <andreas@barkov.dk>
2021-03-04 14:54:49 +0100

    new login page

--- src/__tests__/Login.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Login from "../pages/Login";
-
-test("if NavBar renders with the text 'Home'", () => {
-	render(<Login />);
-	var text = screen.getByText(/login/i);
-	expect(text).toBeInTheDocument();
-});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,5 @@
 // src/pages/Login.js
 import "./Login.scss";
-import { Link } from "@reach/router";
 import querystring from "querystring";
 
 export default function Login() {
@@ -8,30 +7,13 @@ export default function Login() {
 		response_type: "code",
 		client_id: "c153db9d84c8431887a6bb2ac07b25a1",
 		scope: "user-read-private user-read-email",
-		redirect_uri: process.env.NODE_ENV === "production" ? "https://iplaymusic-andreasrisager.netlify.app/callback" : "http://localhost:8888/callback",
+		redirect_uri: process.env.NODE_ENV ? "https://iplaymusic-andreasrisager.netlify.app/callback" : "http://localhost:8888/callback",
 		state: "shu398f3890ujasnd3shf3sf47892s998sfsjhb83xfn982df98sud63uasjnh3ehfisa7",
 	});
 	return (
 		<main className="loginPage">
 			<h1 className="loginPage__heading">Log In</h1>
-			<article>
-				<a href={`https://accounts.spotify.com/authorize?${queryParameters}`}>Log in with spotify</a>
-				<form className="loginPage__form">
-					<label htmlFor="username">Username</label>
-					<input type="text" id="username" name="username" placeholder="Enter your username" />
-					<label htmlFor="password">Password</label>
-					<input type="password" id="password" name="password" placeholder="Enter your password" />
-					<Link to="/" type="submit">
-						Log in
-					</Link>
-				</form>
-				<div className="loginPage__touch">
-					<button className="loginPage__touchBtn">
-						<img src="../images/finger-print.png" alt="finger print" />
-					</button>
-					<p>One-Touch Login</p>
-				</div>
-			</article>
+			<a href={`https://accounts.spotify.com/authorize?${queryParameters}`}><i className="fab fa-spotify"></i>Log in with spotify</a>
 		</main>
 	);
 }
