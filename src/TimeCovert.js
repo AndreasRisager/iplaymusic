e44eb5572c63159d6ea962f9881db401db6ddde5
Andreas Desktop <andreas@barkov.dk>
2021-03-04 12:37:12 +0100

    fix timeconvert

--- src/TimeCovert.js
+++ src/TimeCovert.js
@@ -1,7 +1,4 @@
 export default function TimeCovert(ms) {
-	if (Object.prototype.toString.call(ms) !== "[object Number]") {
-		throw new Error("You must provide function with numbers")
-	}
 	var minutes = Math.floor(ms / 60000);
 	var seconds = ((ms % 60000) / 1000).toFixed(0);
 	return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
